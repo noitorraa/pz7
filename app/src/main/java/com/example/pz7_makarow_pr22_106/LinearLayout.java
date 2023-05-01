18f52ea6dbdbfba159fab654d3eb58c035c0ee25
egor <porem.578@gmail.com>
2023-05-01 19:21:32 +0700

    start

---
+++ app/src/main/java/com/example/pz7_makarow_pr22_106/LinearLayout.java
@@ -0,0 +1,26 @@
+package com.example.pz7_makarow_pr22_106;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LinearLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+        Button button = findViewById(R.id.button9);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LinearLayout.this,TableLayout.class);
+                LinearLayout.this.startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/pz7_makarow_pr22_106/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.pz7_makarow_pr22_106;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBox.isChecked() == true) {
+                    checkBox.setTextColor(Color.BLACK);
+                    Intent intent = new Intent(MainActivity.this, LinearLayout.class);
+                    MainActivity.this.startActivity(intent);
+
+                }
+                else
+                {
+                    checkBox.setTextColor(Color.RED);
+                }
+
+            }
+        });
+
+    }
+
+
+
+}
